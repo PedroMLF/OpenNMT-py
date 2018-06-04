7b53145b7c13a720ab8a18757f9dae0157d46633
PedroMLF <ferreira.ml.pedro@gmail.com>
2018-06-04 12:54:36 +0000

    New experiment

--- onmt/translate/Translator.py
+++ onmt/translate/Translator.py
@@ -158,9 +158,9 @@ class Translator(object):
 
         # ADDED --------------------------------------------------------------
         # Load the translation pieces list
-        home_path = "/home/pmlf/Documents/github/OpenNMT-py-fork/"
-        #home_path = "/home/ubuntu/OpenNMT-py-fork/"
-        tp_path = home_path + "extra_data/translation_pieces_10.pickle"
+        #home_path = "/home/pmlf/Documents/github/OpenNMT-py-fork/"
+        home_path = "/home/ubuntu/OpenNMT-py-fork/"
+        tp_path = home_path + "extra_data/translation_pieces_10-th0pt5.pickle"
         translation_pieces = pickle.load(open(tp_path, 'rb'))
         tot_time = 0
         # END ----------------------------------------------------------------
